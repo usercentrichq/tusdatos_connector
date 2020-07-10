4271b206579472a13bed1db88f62efcbb141aa70
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2020-07-09 22:40:02 -0500

    Running black.

--- setup.py
+++ setup.py
@@ -4,44 +4,48 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['requests>=2.24.0' ]
+requirements = ["requests>=2.24.0"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Sebastian Reyes Espinosa",
-    author_email='sebaslander@gmail.com',
-    python_requires='>=3.6',
+    author_email="sebaslander@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python connector to the webservice of Colombian company TusDatos.co",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='tusdatos_connector',
-    name='tusdatos_connector',
-    packages=find_packages(include=['tusdatos_connector', 'tusdatos_connector.*']),
+    keywords="tusdatos_connector",
+    name="tusdatos_connector",
+    packages=find_packages(include=["tusdatos_connector", "tusdatos_connector.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/sebastian-code/tusdatos_connector',
-    version='1.0.0',
+    url="https://github.com/sebastian-code/tusdatos_connector",
+    version="1.0.0",
     zip_safe=False,
 )
--- tusdatos_connector/__init__.py
+++ tusdatos_connector/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for TusDatos Connector."""
 
 __author__ = """Sebastian Reyes Espinosa"""
-__email__ = 'sebaslander@gmail.com'
-__version__ = '1.0.0'
+__email__ = "sebaslander@gmail.com"
+__version__ = "1.0.0"
